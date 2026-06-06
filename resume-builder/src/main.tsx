eeac48abe48a44a4a4b2ef5ea7c1f525543f581f
Егор Агейкин <stimch@MacBook-Pro-Egor.local>
2026-06-06 10:15:59 +0300

    test render

--- resume-builder/src/main.tsx
+++ resume-builder/src/main.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+/*import { createRoot } from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
 import { AppConfig } from './AppConfig.tsx';
 
@@ -9,3 +9,12 @@ createRoot(document.getElementById('root')!).render(<AppConfig />);
 if (import.meta.env.MODE === 'development') {
   import('./eruda.ts');
 }
+*/
+
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(
+  <div>
+    <h1>TEST OK</h1>
+  </div>
+);
