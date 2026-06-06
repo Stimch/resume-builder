2021f230820a4b5a8b27d6ecf760b14c5f705966
stimch <ageykinegor@mail.ru>
2026-06-06 14:56:48 +0300

    restore app

--- resume-builder/src/main.tsx
+++ resume-builder/src/main.tsx
@@ -12,7 +12,11 @@ if (import.meta.env.MODE === 'development') {
 */
 
 import { createRoot } from 'react-dom/client';
+import vkBridge from '@vkontakte/vk-bridge';
+import { AppConfig } from './AppConfig';
+
+vkBridge.send('VKWebAppInit');
 
 createRoot(document.getElementById('root')!).render(
-  <h1>TEST OK</h1>
+  <AppConfig />
 );
